82f3d0fe38fff39d9a716a5d83c32cf38677a496
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-04 01:15:22 +0100

    Added OmniAuth for Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  config.omniauth :facebook, Rails.application.secrets.fb_APP_ID,
+                             Rails.application.secrets.fb_APP_SECRET
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is

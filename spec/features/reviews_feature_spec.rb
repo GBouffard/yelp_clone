d96e6ff483c92cd47689664c7b78e7a340e30901
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-06 20:46:17 +0100

    Average rating - part 3 - with multiple reviews

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -5,27 +5,37 @@ feature 'reviewing' do
   before do
     user_signs_in
     user_creates_restaurant
-    user_leaves_review
   end
 
   scenario 'allows users to leave a review using a form' do
+    user_leaves_review
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
 
   scenario 'a user cannot leave a 2nd review on a restaurant he/she already reviewed' do
+    user_leaves_review
     expect(page).not_to have_link 'Review KFC'
   end
 
   scenario 'only the creator of a review can delete this review' do
+    user_leaves_review
     expect(page).to have_link 'Delete KFC review'
     click_link 'Delete KFC review'
     expect(page).to have_link 'Review KFC'
   end
 
   scenario 'a user cannot delete a review that he/she did not create' do
+    user_leaves_review
     other_user_signs_in
     expect(page).not_to have_link 'Delete KFC review'
     expect(page).to have_link 'Review KFC'
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    other_user_signs_in
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
   end
 end

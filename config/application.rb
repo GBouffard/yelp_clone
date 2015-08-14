7766ce7142ad9bb0236409f7b9f1d814c8b021b3
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-08-14 18:54:14 +0100

    moved a few things for Heroku

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,11 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require 'coveralls'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Coveralls.wear!
 
 module Yelp
   class Application < Rails::Application
